418c4cf8b226417d108e68411068bed4a88f5d3e
KLacerda08 <kaldmr7880@gmail.com>
2020-09-29 22:30:25 -0400

    updated code, added results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -77,10 +77,36 @@ with open(election_test) as csvfile:
 
     #find max value and get the winner:
     winner = list(results_dict.keys())[list(results_dict.values()).index(maxvalue)]
-    print(winner)
+    #print(winner)
 
 
+#print results to terminal
+    print("Election Results")
+    print("--------------------------")
+    print("Total Votes = " + str(VoteCount))
+    print("--------------------------")
+    print("Khan: " + str(Khan_percent) + " (" + str(Khan_votes) + ")")
+    print("Correy: " + str(Correy_percent) + " (" + str(Correy_votes) + ")")
+    print("Li: " + str(Li_percent) + " (" + str(Li_votes) +")")
+    print("O'Tooley: " + str(O_Tooley_percent) + " (" + str(O_Tooley_votes) +")")
+    print("--------------------------")
+    print("Winner: " + str(winner))
+    
 
+#print results to textfile
+with open(os.path.join('Analysis', 'Election_Results.txt'), "w") as text_file:
+    text_file.write("Election Results\n")
+    text_file.write("--------------------------\n")
+    text_file.write("Total Votes = " + str(VoteCount) + '\n')
+    text_file.write("--------------------------\n")
+    text_file.write("Khan: " + str(Khan_percent) + " (" + str(Khan_votes) +")\n")
+    text_file.write("Correy: " + str(Correy_percent) + " (" + str(Correy_votes) +")\n")
+    text_file.write("Li: " + str(Li_percent) + " (" + str(Li_votes) +")\n")
+    text_file.write("O'Tooley: " + str(O_Tooley_percent) + " (" + str(O_Tooley_votes) +")\n")
+    text_file.write("--------------------------\n")
+    text_file.write("Winner: " + str(winner))
+    text_file.flush()
+    text_file.close()
  
        
 
